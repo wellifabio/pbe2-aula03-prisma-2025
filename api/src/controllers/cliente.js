6797ffbadd545b96eed2ea1f8f14269f39263d27
wellifabio <wellifabio@hotmail.com>
2025-03-25 11:29:25 -0300

    crud cliente

--- api/src/controllers/cliente.js
+++ api/src/controllers/cliente.js
@@ -1,35 +1,43 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const create = async (req, res)=>{
+const create = async (req, res) => {
     const cliente = await prisma.cliente.create({
         data: req.body
     });
     res.status(201).json(cliente).end();
 }
 
-const read = async (req,res)=>{
+const read = async (req, res) => {
     const clientes = await prisma.cliente.findMany();
     res.json(clientes);
 }
 
-const update = async (req, res)=>{
-    const cliente = await prisma.cliente.update({
-        data: req.body,
-        where:{
-            id: Number(req.params.id)
-        }
-    });
-    res.status(204).json(cliente).end();
+const update = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.update({
+            data: req.body,
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(202).json(cliente).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
 }
 
-const remove = async (req, res)=>{
-    const cliente = await prisma.cliente.delete({
-        where:{
-            id: Number(req.params.id)
-        }
-    });
-    res.status(204).json(cliente).end();
+const remove = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(204).json(cliente).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
 }
 
 module.exports = {
--- api/src/router.js
+++ api/src/router.js
@@ -10,6 +10,6 @@ router.get('/',(req, res)=>{
 router.post('/clientes',Cliente.create);
 router.get('/clientes',Cliente.read);
 router.patch('/clientes/:id',Cliente.update);
-router.delete('/clientes/:id',Cliente.delete);
+router.delete('/clientes/:id',Cliente.remove);
 
 module.exports = router;
