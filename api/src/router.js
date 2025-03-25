5048343471bbd6b1859be6696f4debbb32862e4f
wellifabio <wellifabio@hotmail.com>
2025-03-25 10:56:36 -0300

    router atualizado

--- api/src/router.js
+++ api/src/router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const Cliente = require('./controllers/cliente');
+
 router.get('/',(req, res)=>{
     res.json({titulo:'SNOOPY PetSHop API'});
 });
+
+router.post('/clientes',Cliente.create);
+router.get('/clientes',Cliente.read);
 
 module.exports = router;
